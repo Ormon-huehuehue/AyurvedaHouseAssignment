ac8fc01def6e170f344cb7fe34dec5d151cf5818
Ormon-huehuehue <armnvrma10@gmail.com>
2024-12-12 14:36:19 +0530

    basic setup

---
+++ src/app/components/CustomCarousel.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CustomCarousel = () => {
+  return (
+    <div className = "flex">
+      <div className= 'bg-white'>hello</div>
+      <div className = ''></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default CustomCarousel
